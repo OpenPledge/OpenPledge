2c0922e6e70ab0d319cc62c62d408623b5f5d594
Shane LeBlanc <shane@xs.vc>
2018-08-31 11:14:29 -0700

    working wishlist component with api

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Dashboard from './Dashboard';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Wishlist from './Components/Wishlist/Wishlist.js'
 
-ReactDOM.render(<Dashboard />, document.getElementById('root'));
+ReactDOM.render(<Wishlist />, document.getElementById('root'));
 registerServiceWorker();
