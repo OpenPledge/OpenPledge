6305c998a4cada84e9a510b047ca01156f58b022
Liam <you@example.com>
2018-10-05 15:41:35 -0700

    building out Log_in responses

--- src/Components/Login/Beginning_of_a_login_document.js
+++ src/Components/Login/Beginning_of_a_login_document.js
@@ -9,10 +9,25 @@ class Log_In extends Component {
     redirectTo: null
   }
 
-  LogIn = () => {
+  Log_In = () => {
     const data = {
       username: this.state.username,
       password: this.state.password,
     };
+    fetch(url, {
+      method: "POST",
+      headers: {
+        '': ''
+      },
+      body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(responseData => {
+        console.log("response", responseData);
+        if (responseData.superSuccessDog) {
+          this.props.onSuccessfulLogIn({
+            isLoggedIn: true,
+            data: data,
+          })  
 
 export default Log_In;
