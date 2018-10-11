eceb2e7120c4779d0c1d75cbb9b25d910840de6d
Liam <you@example.com>
2018-10-10 23:54:12 -0700

    login render update

--- src/Components/Login/Beginning_of_a_login_document.js
+++ src/Components/Login/Beginning_of_a_login_document.js
@@ -73,3 +73,35 @@ class Log_In extends Component {
   }
 }
 export default Log_In;
+
+render() {
+  if (this.state.redirectTo) {
+    return <Redirect to={this.state.redirectTo} render={(props) =>
+      <Browse {...props} isLoggedIn={this.state.isLoggedIn} />
+    } />
+  } else {
+  return (
+    <div className="LogIn">
+      <div className="InputFields">
+        <h1 className="LogIn--title">Welcome Back!</h1>
+          <h2>username:
+          <input
+            placeholder="enter your username"
+            onChange={this.onUsernameChange}
+          />
+          </h2>
+          <h2>password:
+          <input
+            type="password"
+            placeholder="enter your password"
+            onChange={this.onPasswordChange}
+          />
+          </h2>
+        <Button onClick={this.onLogIn}>log in</Button>
+      </div>
+    </div>
+  );
+}
+}
+}
+export default Log_In;
